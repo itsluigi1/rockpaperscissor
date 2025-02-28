3aa091063567b40cba6ee09ca6c406a9dcaa8ad9
luigigiacomuzzi <giacomuzzi1995gmail.com>
2025-02-28 06:29:46 +0000

    starting code for rock paper scissor game

---
+++ game.js
@@ -0,0 +1,18 @@
+//user imputs choice
+//Computer makes a random choice out of three options
+
+
+function getComputeChoice() {
+
+}
+
+function getHumnaChoice() {
+
+}
+
+var humanScore = 0;
+    computeScore = 0
+
+function playRound() {
+    
+}
