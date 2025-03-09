e80187288bda49aba211f5d11240a26aff5923d9
luigigiacomuzzi <giacomuzzi1995gmail.com>
2025-03-09 18:13:39 +0000

    build up and prettify code

--- game.js
+++ game.js
@@ -1,13 +1,27 @@
 //user imputs choice
 //Computer makes a random choice out of three options
+const choices = ["rock","paper","scissor "]
+
+
 function game() {
+    playRound();
+    //play the game
+    // play five rounds
+    //console based
 
 }
 
-function playRound(playerSelection, computerSelection) {
-console.log(playerSelection)
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computeChoice();
 }
-const playerChoice = "rock"
-const comp = "paper"
 
-playRound(playerChoise,comp)
+function playerChoice() {
+    let input = prompt('Type Rock, Paper, Scissor')
+}
+
+function computeChoice() {
+return choices[Math.floor(Math.random()*choices.lenght)]
+}
+
+game ();
